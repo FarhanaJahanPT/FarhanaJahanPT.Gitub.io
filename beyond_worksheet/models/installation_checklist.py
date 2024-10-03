97ecf5f8415f90fbf625b4901ee4c44bde5eaf44
cybroathul <athul@cybrosys.info>
2024-10-03 12:29:01 +0530

    [UPDT] selfie seletion field

--- beyond_worksheet/models/installation_checklist.py
+++ beyond_worksheet/models/installation_checklist.py
@@ -7,8 +7,10 @@ class InstallationChecklist(models.Model):
     _description = "Installation Checklist"
 
     name = fields.Char(string='Name', required=True)
-    task_ids = fields.Many2many('project.task', domain=[('x_studio_type_of_service', '=', 'New Installation')], string='Task')
-    type = fields.Selection([('img', 'Image/PDF'), ('text', 'Text')],string='Type', default='img')
+    task_ids = fields.Many2many('project.task', domain=[('x_studio_type_of_service', '=', 'New Installation')],
+                                string='Task')
+    type = fields.Selection([('img', 'Image/PDF'), ('text', 'Text')], string='Type', default='img')
     compulsory = fields.Boolean(string='Compulsory', default=False)
     min_qty = fields.Integer(string='Minimum Quantity', default=0)
-    is_selfie = fields.Boolean(string="Selfie", default=False)
+    selfie_type = fields.Selection([('check_in', 'Check In'), ('mid', 'Mid Time'), ('check_out', 'Check Out')],
+                                   string='Selfie Type')
--- beyond_worksheet/models/service_checklist.py
+++ beyond_worksheet/models/service_checklist.py
@@ -8,7 +8,8 @@ class ServiceChecklist(models.Model):
 
     name = fields.Char(string='Name', required=True)
     task_ids = fields.Many2many('project.task', domain=[('x_studio_type_of_service', '=', 'Service')], string='Task')
-    type = fields.Selection([('img', 'Image/PDF'), ('text', 'Text')],string='Type', dfault='img')
+    type = fields.Selection([('img', 'Image/PDF'), ('text', 'Text')], string='Type', dfault='img')
     compulsory = fields.Boolean(string='Compulsory', dfault=False)
     min_qty = fields.Integer(string='Minimum Quantity', dfault=0)
-    is_selfie = fields.Boolean(string="Selfie", default=False)
+    selfie_type = fields.Selection([('check_in', 'Check In'), ('mid', 'Mid Time'), ('check_out', 'Check Out')],
+                                   string='Selfie Type')
